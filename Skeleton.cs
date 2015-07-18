ca78361efbea66b9f860c0ab83cd2a0b421bf356
magnusi <luk.hozda@gmail.com>
2015-07-18 23:57:04 +0200

    This is the next step. Added comments and a command.

--- Skeleton.cs
+++ Skeleton.cs
@@ -8,16 +8,21 @@ using TerrariaApi.Server;
 using TShockAPI;
 using System.Reflection;
 
+//Namespace of our plugin
 namespace Skeleton
 {
+    //Api tick
     [ApiVersion(1,19)]
     public class ToyBoat : TerrariaPlugin
     {
-        public ToyBoat(Main game)
+        
+ //Constructor
+        public ToyBoat(Terraria.Main game)
             : base(game)
         {
 
         }
+        //Author of the plugin
         public override string Author
         {
             get
@@ -25,6 +30,7 @@ namespace Skeleton
                 return "ToyBoat";
             }
         }
+        //Name of the plugin
         public override string Name
         {
             get
@@ -32,6 +38,7 @@ namespace Skeleton
                 return "Skeleton";
             }
         }
+        //Description of the plugin
         public override string Description
         {
             get
@@ -39,6 +46,7 @@ namespace Skeleton
                 return "Plugin Skeleton";
             }
         }
+        //Version of the plugin. We are fetching it automatically, so no need to worry about changing anything here
         public override Version Version
         {
             get
@@ -46,9 +54,16 @@ namespace Skeleton
                 return Assembly.GetExecutingAssembly().GetName().Version;
             }
         }
+        //Initialize function. Executed upon loading the plugin. Most important function out there.
         public override void Initialize()
         {
-            
+            //This is where we are going to work at next
+             TShockAPI.Commands.ChatCommands.Add(new Command("bert.is.noob", BertIsNoob, "bert"));
+        }
+
+        private void BertIsNoob(CommandArgs args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
