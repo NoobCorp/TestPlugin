a0384b8c553727d3d37873af62e3c6953cad570e
CrazyLegsSteph <crazylegssteph@gmail.com>
2015-07-18 16:47:30 -0500

    Learning how to push a commit

---
+++ Skeleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using System.Reflection;
+
+namespace Skeleton
+{
+    [ApiVersion(1,19)]
+    public class ToyBoat : TerrariaPlugin
+    {
+        public ToyBoat(Main game)
+            : base(game)
+        {
+
+        }
+        public override string Author
+        {
+            get
+            {
+                return "ToyBoat";
+            }
+        }
+        public override string Name
+        {
+            get
+            {
+                return "Skeleton";
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "Plugin Skeleton";
+            }
+        }
+        public override Version Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+        public override void Initialize()
+        {
+            
+        }
+    }
+}
