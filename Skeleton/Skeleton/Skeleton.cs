ba95912a8f49204e352dde939b2dd6b999b20982
CrazyLegsSteph <crazylegssteph@gmail.com>
2015-07-18 15:41:16 -0500

    test

---
+++ Skeleton/Skeleton/Skeleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Skeleton
+{
+    [ApiVersion(1,19)]
+    public class ToyBoat : TerrariaPlugin
+    {
+        public ToyBoat(Main game)
+            : base(game)
+        {
+
+        }
+        public override string Author
+        {
+            get
+            {
+                return "ToyBoat";
+            }
+        }
+        public override string Name
+        {
+            get
+            {
+                return "Skeleton";
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "Plugin Skeleton";
+            }
+        }
+        public override Version Version
+        {
+            get
+            {
+                return return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+        public override void Initialize()
+        {
+            
+        }
+    }
+}
